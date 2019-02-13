7401e02b2640346d02d1dd94ddf2f1643cad40e5
negashm <47371999+negashm@users.noreply.github.com>
2019-02-13 16:23:05 -0500

    Create program2.cpp

---
+++ week4/program2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char str[30][40];
+	int row;
+	int col;
+	
+	for (row =0; row < 2; row++){
+		for (col = 0; col < 3; col++) {
+			cout << "Students, please enter your full name" << endl;
+		}
+	}
+
+	return 0;
+}
