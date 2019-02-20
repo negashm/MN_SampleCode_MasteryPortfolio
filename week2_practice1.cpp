6a695185cd746cbe0599a13ee95cdceef9b1975c
negashm <47371999+negashm@users.noreply.github.com>
2019-02-20 17:27:05 -0500

    Week 2

---
+++ week2_practice1.cpp
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int num;
+	for (;;)
+	{
+		cout << "Please enter a positive number to find its value when squared. Enter 0 to quit:";
+		cin >> num;
+
+		if (num >= 1)
+		{
+			cout << " Your value squared is: ";
+			cout << num * num << endl;
+		}
+		else if (num <= -1)
+		{
+			cout << "Please enter a positive value!" << endl;
+		}
+		else if (num == 0)
+		{
+			break;
+		}
+
+	}
+
+}
