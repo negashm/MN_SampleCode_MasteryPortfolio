de48b612e29f9c04b52a5245ecd71bf1decb9380
negashm <47371999+negashm@users.noreply.github.com>
2019-05-08 11:58:53 -0400

    Update program1.cpp

--- week9/program1.cpp
+++ week9/program1.cpp
@@ -1,155 +1,72 @@
 //Write a program to prompt the user for vehicle specifications. 
 
+#include "pch.h"
+#include <cstdio>
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 using namespace std;
 
-//Define class for vehicles 
-
-class road_vehicle
+int main() 
 {
-	int wheels;
-	int passengers;
-	string vehicle;
-	string car;
-  
-public:
-	void set_wheels(int num) {
-		wheels = num;
-	}
-	int get_wheels() {
-		return wheels;
-	}
-	void set_passengers(int num) {
-		passengers = num;
-	}
-	int get_passengers() {
-		return passengers;
-	}
-	void set_vtype(string str)
-	{
-		vehicle = str;
-	}
-	string get_vtype()
-	{
-		return vehicle;
-	}
-
-	void set_str(string str)
-	{
-		car = str;
-	}
-
-	string get_str()
-	{
-		return car;
-	}
-};
-
-//Define truck
-class truck : public road_vehicle {
-	int cargo;
-public:
-	void set_cargo(int size) {
-		cargo = size;
-	}
-	int get_cargo() {
-		return cargo;
-	}
-};
-//Define Automobile
-enum type { car, van, wagon };
-class automobile : public road_vehicle {
-	enum type car_type;
-public:
-	void set_type(type t) { car_type = t; }
-	enum type get_type() { return car_type; }
-};
-
-//Node linked list below 
-class node
-{
-public:
-	road_vehicle data;
-	node *next;
-	node()
-	{
-		next = NULL;
-	}
-	void insert(road_vehicle data);
-	void print_data();
-};
-
-ostream &operator<<(ostream &stream, road_vehicle data)
-{
-	stream << data.get_vehicle() << endl;
-	stream << data.get_wheels() << endl;
-	stream << data.get_passengers() << endl;
-	stream << data.get_str() << endl;
-	return stream;
-}
-
-void node::print_data() {
-	cout << data;
-
-	if (next != NULL) { return next->print_data(); }
-}
-void node::insert(road_vehicle v) {
-	if (next == NULL) {
-		next = new node;
-		next->data = v;
-	}
-	else {
-		next->insert(v);
-	}
-
-}
-
-int main() {
 	ofstream out;
 	out.open("output.txt");
-	node linked_list;
-	road_vehicle vehicle;
-	string myvalue;
-	int value;
-	while (c)
+	if (!out) 
 	{
-		cout << "Is this an automobile or a truck? Enter 'automobile' or 'truck'" << endl;
-		getline(cin, myvalue);
-		vehicle.set_vehicle(myvalue);
-		if (myvalue == "truck")
+		cout << "Cannot open file." << endl;
+		return 1;
+	}
+	if (!out.is_open()) 
+	{
+		cout << "File is not open." << endl;
+		return 2;
+	}
+
+	cout << "VEHICLE LIST" << endl;
+	int vehiclenum = 0;
+	for (;;)
+	{ 
+		string type;
+		int wheels;
+		int passengers;
+		int cargolbs;
+		string type2;
+		string exit;
+
+		vehiclenum += 1;
+		cout << "Is this an automobile or a truck? Enter 'automobile' or 'truck' ";
+		cin >> type;
+		out << "Vehicle ";
+		out << vehiclenum << endl;
+		out << "Type: ";
+		out << type << endl;
+
+		cout << "How many wheels does this vehicle have? ";
+		cin >> wheels;
+		out << "Wheels: ";
+		out << wheels << endl;
+
+		cout << "How many passengers can this vehicle hold? ";
+		cin >> passengers;
+		out << "Passenger Capacity: ";
+		out << passengers << endl;
+
+		if (type == "truck")
 		{
-			cout << "How many wheels does this vehicle have?" << endl;
-			cin >> value;
-			vehicle.set_wheels(value);
-
-			cout << "How many passengers can this vehicle hold?" << endl;
-			cin >> value;
-			vehicle.set_passengers(value);
-
-			cout << "How much cargo (in pounds) can this vehicle hold?" << endl;
-			cin >> value;
-			string i = to_string(value);
-			vehicle.set_str(i);
-    }
-   	else
-  	{
-			cout << "How many wheels does this vehicle have?" << endl;
-			cin >> value;
-			vehicle.set_wheels(value);
-
-			cout << "How many passengers can this vehicle hold?" << endl;
-			cin >> value;
-			vehicle.set_passengers(value);
-
-			cin.ignore();
-			cout << "What type of automobile is this?   Enter van, car or wagon" << endl;
-			getline(cin, myvalue);
-			vehicle.set_str(myvalue);
+			cout << "How much cargo (in pounds) can this vehicle hold? ";
+			cin >> cargolbs;
+			out << "Cargo Capacity (lbs): ";
+			out << cargolbs << endl;
 		}
-		out << vehicle;
-		linked_list.insert(vehicle);
+
+		if (type == "automobile")
+		{
+			cout << "What type of automobile is this?   Enter van, car or wagon ";
+			cin >> type2;
+			out << "Automobile type: ";
+			out << type2 << endl;
+		}
 
 		cout << "Would you like to enter another vehicle? yes or no: ";
 		cin >> exit;
@@ -165,11 +82,18 @@ int main() {
 		}
 	
 	}
+	string line;
+	ifstream file("output.txt");
+	if (file.is_open())
+	{
+		while (getline(file, line))
+		{
+			cout << line << endl;
+		}
+		file.close();
+	}
 
-	linked_list.print_data();
-  
-  	else cout << "Unable to open";
+	else cout << "Unable to open file";
 	out.close();
-	system("pause");
 	return 0;
 }
